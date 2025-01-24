3891309970c709f542392fdc139ca041a41e165a
Gustavo1701 <gustavojr.lg.10@gmail.com>
2025-01-23 21:33:03 -0300

    Fazendo a requisição no banco e exibindo um json com os valores adicionado no bando

--- db.js
+++ db.js
@@ -1,19 +1,13 @@
 import pg from 'pg';
-const {Pool} = pg;
+const { Pool } = pg;
 
-const pool = new Pool({
+export const pool = new Pool({
     user: 'neondb_owner',
     password: 'npg_3Ifv6DJZlXzY',
     host: 'ep-still-snowflake-a5ncrhlo-pooler.us-east-2.aws.neon.tech',
     port: '5432',
     database: 'neondb',
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
     },
 });
-
-const result = await pool.query('SELECT NOW()');
-const rows = result.rows[0].now;
-console.log(rows);
-
-
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-import { fastify } from 'fastify'
+import { fastify } from 'fastify';
+import { pool } from './db.js'; 
 
 const server = fastify();
 
-server.get('/', () => {
-    return('Servidor rodando.')
+// Rota para consultar a hora no banco de dados
+server.get('/pastel', async (req, res) => {
+    try {
+        const consulta = await pool.query('SELECT * FROM pastel'); 
+
+        res.send({
+            menssage: 'Retornou pasteis',
+            data: consulta.rows,
+        });
+
+    } catch (err) {
+        res.status(500).send('Erro ao consultar o banco de dados.');
+    }
 });
 
-server.listen({
-    port:3000,
+
+// Inicializa o servidor
+server.listen({ port: 3000 }).then(() => {
+    console.log(`Servidor rodando na porta: 3000`);
+}).catch(err => {
+    console.error(err);
+    process.exit(1);
 });
